5c8c9b293472953609c6b2c81468fd07f1e61c38
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-05-21 16:53:06 +0530

    updated

---
+++ src/datatype.java
@@ -0,0 +1,27 @@
+
+import java.lang.String;
+class Datatype {
+    public static void datatype(String[] args){
+        // Primitive Data type
+        byte b =8;
+        short s =78;
+        int i =4816;
+        long l =9526526526L;
+        float f =5953.8f;
+        double d =952.9852695;
+        char c= 'R';
+        boolean q= true;
+
+        // Size and range of data type:
+        System.out.println("Int Min value: "+Integer.MIN_VALUE);
+        System.out.println("Int Max value: "+Integer.MAX_VALUE);
+        System.out.println("Int BYTES: "+Integer.BYTES);
+
+        System.out.println("Float Min value: "+Float.MIN_VALUE);
+        System.out.println("Float Max value: "+Float.MAX_VALUE);
+        System.out.println("Float BYTES: "+Float.BYTES);
+
+        // Integer to binary
+        System.out.println("Integer to binary is: "+Integer.toBinaryString(i));
+    }
+}
