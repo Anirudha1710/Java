cea187130570c1447f0733d389ed4accb2e7b9b3
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-05-30 16:48:00 +0530

    updated

---
+++ src/Printing_Format_and_String/Example2.java
@@ -0,0 +1,19 @@
+package Printing_Format_and_String;
+
+public class Example2 {
+    public static void main(String[] args) {
+        //Removes special characters:
+        String s="ghgd$!#!#$Q%#11567";
+        System.out.println(s.replaceAll("[^a-zA-z0-9]",""));
+
+        //Removes free spaces:
+        String s1="   asdd  sdsd deddsf  ";
+        System.out.println(s1.replaceAll("\\s+"," ").trim());
+
+        // counts number of strings:
+        String s2=" sdj   wjke dyebi ug g u  iwdffuyasjgdfy   ";
+        s2=s2.replaceAll("\\s+"," ").trim();
+        String words[]=s2.split("\\s");
+        System.out.println(words.length);
+    }
+}
