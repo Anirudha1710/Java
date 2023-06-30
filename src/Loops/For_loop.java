899df5233b6f0b2147f31fb37f56e19bd6c7947e
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-06-30 22:11:44 +0530

    updated

---
+++ src/Loops/For_loop.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class For_loop {
+    public static void main(String[] args) {
+        for(int i=1; i<100;){
+            System.out.println(i);
+            i=i*2;
+        }
+    }
+}
