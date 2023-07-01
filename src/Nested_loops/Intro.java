cb10ad8e91b32ee70fdac6eef8d36428ceaf1d00
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-07-01 21:34:09 +0530

    updated

---
+++ src/Nested_loops/Intro.java
@@ -0,0 +1,11 @@
+package Nested_loops;
+public class Intro {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.println("("+i+","+j+")");
+            }
+            System.out.println();
+        }
+    }
+}
