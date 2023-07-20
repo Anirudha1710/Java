f406a2d0b152d0220518946caadaaae98989d4ef
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-07-20 21:21:03 +0530

    updated

--- src/Arrays/Deleting_an_array_element.java
+++ src/Arrays/Deleting_an_array_element.java
@@ -1,5 +1,45 @@
 package Arrays;
 
-public class Deleting_an_array_element {
+import java.util.Scanner;
 
+public class Deleting_an_array_element {
+    public static int[] TakeUserInput(){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("\nEnter size of array: ");
+        int size = s.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("\nEnter array element: ");
+        for(int i = 0; i< size; i++){
+
+            arr[i] = s.nextInt();
+        }
+        return arr;
+    }
+    public static   void    Delete(int[] arr,int pos) {
+        int x = 0;
+        int size = arr.length;
+        if (pos >= 0 && pos < arr.length) {
+            x = arr[pos];
+
+            for (int i = pos; i < size; i++) {
+                arr[i] = arr[i + 1];
+            }
+            size--;
+
+            for (int i = 0; i < size; i++) {
+                System.out.println(arr[i] + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] arr=TakeUserInput();
+        System.out.println("Enter position: ");
+        int pos=s.nextInt();
+        Delete(arr,pos);
+    }
 }
