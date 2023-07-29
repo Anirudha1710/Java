95bef1d4440932db766682a10fb0825136b442e3
Anirudha1710 <114614344+Anirudha1710@users.noreply.github.com>
2023-07-29 22:04:23 +0530

    updated

---
+++ src/Arrays/Arrays_Basics.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Arrays_Basics {
+    public static void main(String[] args) {
+        // for loop :
+        int[] A ={1,2,3,4,5,6};
+
+        System.out.print("\nFor loop se: [");
+        for (int i = 0; i < A.length; i++) {
+            System.out.print(" "+A[i]);
+        }
+        System.out.print("]");
+
+        // For-each Loop:
+        int[] arr = new int[5];
+        arr[0]=5;
+        arr[1]=10;
+        arr[2]=15;
+        arr[3]=20;
+        arr[4]=30;
+
+        System.out.print("\n for each loop se: [");
+        for (int i : arr) {
+            System.out.print(" "+i);
+        }
+        System.out.print("]");
+    }
+}
